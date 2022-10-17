e889112dac328cae462b0f41105a746824b2afe5
Dipu Dey <dipudey764@gmail.com>
2022-10-17 23:44:12 +0600

    add express js simple routing system

--- main.js
+++ main.js
@@ -7,6 +7,33 @@ app.get('/',(req,res) => {
     res.send("Hello world")
 })
 
+/* app.get("/movie",(req,res) => {
+    res.send("All Movie List");
+}) 
+
+app.post("/movie",(req,res) => {
+    res.json({"data": "Store new move"});
+}) */
+
+
+app.route("/movie")
+    .get((req,res) => {
+        res.send("All Movie List");
+    })
+    .post((req,res) => {
+        res.json({"data": "Store new move"});
+    })
+
+app.get("/movie/:id",(req,res) => {
+    res.send(`Fetching movie id: ${req.params.id}`)
+})
+
+app.all("/*",(req,res) => {
+    res.send("Page not found");
+})
+
+
+
 app.listen(port,() => {
     console.log(`Server is running at localhost:${port}`);
 })

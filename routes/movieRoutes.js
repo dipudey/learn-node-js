a49adcafc798529f22defc4210e6edaef848458f
Dipu Dey <dipudey764@gmail.com>
2022-10-26 23:32:50 +0600

    add response status

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -8,7 +8,7 @@ movieRoutes.route("/")
     .get(async (req,res) => {
         const db = await connect();
         const moviesData = await db.collection("movies").find().toArray();
-        res.json({
+        res.status(200).json({
             data: moviesData
         })
     })
@@ -21,7 +21,7 @@ movieRoutes.route("/")
         }
 
         db.collection('movies').insertOne(data)
-        res.json({"data": "Store new move"});
+        res.status(201).json({"data": "Store new move"});
     })
 
 movieRoutes.route("/:id")
@@ -29,7 +29,7 @@ movieRoutes.route("/:id")
             const _id = ObjectId(req.params.id)
             const db = await connect();
             const data = await db.collection("movies").find({_id}).toArray()
-            res.json(data[0])
+            res.status(200).json(data[0])
         })
         .patch(async (req,res) => {
             const _id = ObjectId(req.params.id)
@@ -39,13 +39,13 @@ movieRoutes.route("/:id")
                 publish_date: req.body.publish_date
             }
             await db.collection("movies").updateOne({_id},{$set: data})
-            res.json({"data": "Update"})
+            res.status(200).json({"data": "Update"})
         })
         .delete(async (req,res) => {
             const _id = ObjectId(req.params.id)
             const db = await connect();
             await db.collection("movies").deleteOne({_id})
-            res.json({data:"movie deleted"})
+            res.status(200).json({data:"movie deleted"})
         })
 
 module.exports = movieRoutes;

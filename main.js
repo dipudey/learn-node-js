6bbfe540526de4ef6c3b08dbb194f06f6a76aff3
Dipu Dey <dipudey764@gmail.com>
2022-10-26 23:08:22 +0600

    dynamic data insert by body parser

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const express = require("express")
 const app = express();
 const PORT = 3000;
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
 // add routes file
 const routes = require("./routes/index")
 app.use(routes)
--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -4,15 +4,19 @@ const movieRoutes = express.Router()
 const connect = require("./../database/db")
 
 movieRoutes.route("/")
-    .get((req,res) => {
-        res.send("All Movie List");
+    .get(async (req,res) => {
+        const db = await connect();
+        const moviesData = await db.collection("movies").find().toArray();
+        res.json({
+            data: moviesData
+        })
     })
     .post(async (req,res) => {
         const db = await connect();
 
         const data = {
-            name: "howa",
-            publish_date: "2022-10-25"
+            name: req.body.name,
+            publish_date: req.body.publish_date
         }
 
         db.collection('movies').insertOne(data)
